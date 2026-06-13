2fc348d7f545c1d051c084917c11319a4663f001
Ing. Losmar Sobalvarro <losmar.scout.xvi@gmail.com>
2026-06-12 20:20:31 -0600

    style
    style, login, ejemplos

---
+++ JavaScript/script.js
@@ -0,0 +1,16 @@
+const btnLogin = document.getElementById("btnLogin");
+const modal =document.getElementById("modalLogin");
+const cerrar = document.querySelector(".cerrar");
+
+btnLogin.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+cerrar.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }});
