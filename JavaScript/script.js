aaa341e44f9f20cb4da4d166974abc877df5e1e0
Ing. Losmar Sobalvarro <losmar.scout.xvi@gmail.com>
2026-06-27 09:04:09 -0600

    actu27/06/26
    se actualizn propiedades

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,19 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
+// const btnLogin = document.getElementById("btnLogin");
+// const modal = document.getElementById("modalLogin");
+// const cerrar = document.querySelector(".cerrar");
+
+// btnLogin.addEventListener("click", () => {
+//     modal.style.display = "flex";
+// });
+
+// cerrar.addEventListener("click", () => {
+//     modal.style.display = "none";
+// });
+
+// window.addEventListener("click", (e) => {
+//     if (e.target === modal) {
+//         modal.style.display = "none";
+//     }});
+// });
+
+const contenido = document.getElementById("contenido");
 const btnLogin = document.getElementById("btnLogin");
 const modal = document.getElementById("modalLogin");
-const cerrar = document.querySelector(".cerrar");
+const cerrar = document.getElementById(".cerrar");
+
 
 btnLogin.addEventListener("click", () => {
     modal.style.display = "flex";
+    contenido.classList.add("blur");
 });
 
 cerrar.addEventListener("click", () => {
     modal.style.display = "none";
+    contenido.classList.remove("blur");
 });
 
 window.addEventListener("click", (e) => {
     if (e.target === modal) {
+
         modal.style.display = "none";
-    }});
+        contenido.classList.remove("blur");
+    }
 });
